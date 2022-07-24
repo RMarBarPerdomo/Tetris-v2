d6822f48cf5c111bd4eddabc4ecc5513f323435c
jwerne <60320525+jwerne@users.noreply.github.com>
2022-07-24 17:47:23 +0200

    all blocks spawn

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -26,8 +26,8 @@ public class Spawn : MonoBehaviour
     // Update is called once per frame
     public void NewPiece()
     {   
-            //Random.Range(0, Pieces.Length)
-            Instantiate(Pieces[2], transform.position, Quaternion.identity);
+            
+            Instantiate(Pieces[Random.Range(0, Pieces.Length)], transform.position, Quaternion.identity);
             FindObjectOfType<Feel>().SetFeelOutLine_false();
             
             //Random.Range(0, Pieces.Length)
